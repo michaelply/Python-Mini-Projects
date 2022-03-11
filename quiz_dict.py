b46bb9042ad32335a9c86a916a303a8c972004e5
Michael Poon <michaelpoon@Michaels-MacBook-Pro.local>
2022-03-11 16:37:25 -0500

    refactor quiz_dict and delete scrape_nba_data

--- quiz_dict.py
+++ quiz_dict.py
@@ -1,34 +1,29 @@
 import constants as CONSTANTS
-import data_processing as data
+import scrape_basketball_reference as scrape
 
 def construct_quiz_dict(question_lists):
     quiz_dict = {}
 
     # Load latest scrapped data
-    standing = data.load_standing_data(CONSTANTS.STANDING_OUTPUT_PATH)
-    basic_stats = data.load_basic_stats_data(CONSTANTS.PLAYER_BASIC_STATS_OUTPUT_PATH, standing)
-    advanced_stats = data.load_advanced_stats_data(CONSTANTS.PLAYER_ADVANCED_STATS_OUTPUT_PATH, standing)
+    leaders_dict = scrape.get_nba_leaders_dict()
 
     for i in range(len(question_lists)):
         question = question_lists[i]
         if question not in quiz_dict:
-            answer = get_answer(question, standing, basic_stats, advanced_stats)
+            answer = get_answer(leaders_dict, question)
             quiz_dict[question] = answer
     return quiz_dict
 
-def get_answer(question, standing, basic_stats, advanced_stats):
+def get_answer(leaders_dict, question):
 
     if question == CONSTANTS.QUESTIONS[0]:
         # Most points per game
-        points = data.get_basic_points_data(basic_stats)
-        answer = data.most_points_per_game(points)
+        answer = leaders_dict["points per game"].loc[0, "name"]
         return answer
     elif question == CONSTANTS.QUESTIONS[1]:
         # Most assists per game
-        assists = data.get_basic_assists_data(basic_stats)
-        answer = data.most_assists_per_game(assists)
+        answer = leaders_dict["assists per game"].loc[0, "name"]
         return answer
     elif question == CONSTANTS.QUESTIONS[2]:
-        rebounds = data.get_basic_rebounds_data(basic_stats)
-        answer = data.most_rebounds_per_game(rebounds)
+        answer = leaders_dict["rebounds per game"].loc[0, "name"]
         return answer
